fb951ef80309821b3c741f7fdee7336da34252a7
marg <144652524+stormweavering@users.noreply.github.com>
2024-01-27 03:36:21 -0500

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World\n");
+        Console.WriteLine("Good day!");
+        Console.WriteLine("{0}", 1);
+        Console.WriteLine("{0}", 101);
+        Console.WriteLine("{0}", 1001);
+    }
+}
